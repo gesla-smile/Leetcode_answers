ff080fd2e06e76c925a8822057a5ec986bab37bf
gesla-smile <116368564+gesla-smile@users.noreply.github.com>
2022-11-30 18:29:02 -0500

    Add files via upload

---
+++ Question_713_Subarray_Products_less_then_K.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numSubarrayProductLessThanK(self, nums, k):
+        L = len(nums)
+        left = 0 
+        right = 0
+        prodct = 1
+        result = 0
+        while right < L:
+            prodct *= nums[right]
+            while prodct >= k and left <= right:
+                prodct /= nums[left]
+                left += 1
+            result = right - left + 1
+            right += right + 1
+        return result

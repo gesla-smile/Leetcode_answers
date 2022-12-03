90d9fc8d76b057f05df9e81f3347aceb664054ec
gesla-smile <116368564+gesla-smile@users.noreply.github.com>
2022-12-02 21:32:00 -0500

    Add files via upload

---
+++ Question_123_Best_time_to_buy_and_sell_stocks.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices):
+        n = len(prices)
+        buy1 = buy2 = -prices[0]
+        sell1 = sell2 = 0
+        for i in range(1, n):
+            buy1 = max(buy1, -prices[i])
+            sell1 = max(sell1, buy1 + prices[i])
+            buy2 = max(buy2, sell1 - prices[i])
+            sell2 = max(sell2, buy2 + prices[i])
+        return sell2
